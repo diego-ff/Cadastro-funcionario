07fa77cb32f95fb59ef48756521215001fae5584
diegoff <diegoff_@gmail.com>
2026-04-25 14:38:08 -0300

    configure postgres database

--- mysite/settings.py
+++ mysite/settings.py
@@ -72,10 +72,9 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 
 # DATABASE
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
 
 
