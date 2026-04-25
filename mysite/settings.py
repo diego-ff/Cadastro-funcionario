ac12b9c808768b61445bff95bc1e7259d37c5736
diegoff <diegoff_@gmail.com>
2026-04-25 14:27:18 -0300

    fix database for render (sqlite)

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,11 +78,6 @@ DATABASES = {
     }
 }
 
-if not DEBUG:
-    DATABASES["default"] = dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
-        conn_max_age=600,
-    )
 
 
 # PASSWORD VALIDATION
