998ffbdb33b7a7bae1930c0bb17fe9887fe4f9be
diegoff <diegoff_@gmail.com>
2026-04-25 14:20:04 -0300

    add home route

--- funcionarios/urls.py
+++ funcionarios/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+from .views import home
+
+urlpatterns = [
+    path("", home),
+]
 
 urlpatterns = [
     path("", views.lista_funcionarios, name="lista_funcionarios"),
--- funcionarios/views.py
+++ funcionarios/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Funcionario
+from django.shortcuts import render
 
 def lista_funcionarios(request):
     funcionarios = Funcionario.objects.all()
     return render(request, "funcionarios/lista.html", {"funcionarios": funcionarios})
+
+def home(request):
+    return render(request, "home.html")
